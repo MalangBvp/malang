e480bdbdf96152704a19f9e45fcf5f563ad7f1bf
multiverseweb <multiverse.letter@gmail.com>
2025-06-27 18:46:58 +0530

    initial commit

---
+++ src/scripts/home.js
@@ -0,0 +1,10 @@
+//function to toggle side bar
+document.addEventListener("DOMContentLoaded", function() {
+    const burger = document.getElementById("burger");
+    const nav = document.querySelector("nav");
+
+    burger.addEventListener("click", function() {
+        nav.classList.toggle("active");
+        burger.classList.toggle("active");
+    });
+});
