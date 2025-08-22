1059629ac847d6a4c65e62594add0d788a369191
multiverseweb <multiverse.letter@gmail.com>
2025-08-22 07:16:42 +0530

    fixed theme toggle

--- src/scripts/home.js
+++ src/scripts/home.js
@@ -1,19 +1,12 @@
-burger = document.getElementById("burger");
-header = document.querySelector("header");
-burger.addEventListener("click", () => {
-    if (window.scrollY < 200) {
-        header.classList.toggle("top");
-        burger.classList.toggle("top");
-    }
-});
 
-//when page is scrolled more than 200px toggle class top on header
-window.addEventListener("scroll", () => {
-    if (window.scrollY > 200) {
-        header.classList.remove("top");
-        burger.classList.remove("top");
-    } else {
-        header.classList.add("top");
-        burger.classList.add("top");
-    }
-});
+function searchContent() {
+  let input = document.getElementById("searchInput").value.toLowerCase();
+  let items = document.querySelectorAll("#contentList li");
+  
+  items.forEach(item => {
+    item.style.display = item.textContent.toLowerCase().includes(input) ? "" : "none";
+  });
+}
+
+
+
